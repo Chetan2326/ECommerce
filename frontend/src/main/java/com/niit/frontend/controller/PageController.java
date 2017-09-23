ad36a5d2a76e38be115fb3831f4405597ff87676
Chetan2326 <chetanpujary@gmail.com>
2017-09-23 12:53:00 +0530

    Update

--- frontend/src/main/java/com/niit/frontend/controller/PageController.java
+++ frontend/src/main/java/com/niit/frontend/controller/PageController.java
@@ -13,11 +13,28 @@ public class PageController {
 	@RequestMapping(value = {"/", "/home", "/index"})
 	public ModelAndView index()	{
 		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting", "Welcome To Spring Web MVC");
+		mv.addObject("title", "Home");
+		mv.addObject("userClickHome", true);
 		return mv;
 	}
 	
-	/*@RequestMapping(value="/test")
+	@RequestMapping(value = {"/about"})
+	public ModelAndView about()	{
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "About Us");
+		mv.addObject("userClickAbout", true);
+		return mv;
+	}
+	
+	@RequestMapping(value = {"Contact"})
+	public ModelAndView contact()	{
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "Contact Us");
+		mv.addObject("userClickContact", true);
+		return mv;
+	}
+	
+	@RequestMapping(value="/test")
 	public ModelAndView test(@RequestParam(value="greeting", required=false) String greeting)	{
 		if(greeting == null) {
 			greeting = "Hello There!";
@@ -25,9 +42,9 @@ public class PageController {
 		ModelAndView mv = new ModelAndView("page");
 		mv.addObject("greeting", greeting);
 		return mv;
-	}*/
+	}
 	
-	@RequestMapping(value="/test/{greeting}")
+	/*@RequestMapping(value="/test/{greeting}")
 	public ModelAndView test(@PathVariable("greeting") String greeting)	{
 		if(greeting == null) {
 			greeting = "Hello There!";
@@ -35,6 +52,6 @@ public class PageController {
 		ModelAndView mv = new ModelAndView("page");
 		mv.addObject("greeting", greeting);
 		return mv;
-	}
+	}*/
 	
 }
